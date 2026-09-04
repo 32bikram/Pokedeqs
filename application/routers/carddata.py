bff5052e14d9a52f99f55c987c43efd8f9582710
32bikram <bikramsarkareveland@gmail.com>
2026-09-04 11:20:56 +0000

    user can view all cards of their collection

--- application/routers/carddata.py
+++ application/routers/carddata.py
@@ -58,3 +58,23 @@ def get_card_data(pokemon_name : Optional[str] = None, set_name : Optional[str] 
             card_data.append(data)
 
         return card_data
+
+@router.get("/mycollection")
+def get_user_collection(db : Session = Depends(database.get_db), current_user = Depends(oauth2.get_current_user)):
+    user_id = current_user.user_id
+    cards_details = (db.query(models.Cards(), models.Collections.card_count)
+                .join(models.Collections, models.Cards.card_id==models.Collections.card_id)
+                .filter(models.Collections.user_id==user_id))
+    if(cards_details==None):
+        raise HTTPException(
+            status_code =  status.HTTP_204_NO_CONTENT,
+            detail = "Your collection us empty"
+        )
+    card_data = []
+    for cards in cards_details:
+        card_obj, card_count = cards
+        data = schemas.Card.model_validate(card_obj).model_dump()
+        data["card_count"] = card_count
+        card_data.append(data)
+    
+    return card_data
